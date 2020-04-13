8a7359a6b24dab2a9359571ed95b10dd06454afb
Elukoye <lukoyedith@gmail.com>
2020-04-13 03:33:55 +0300

    add login to goals app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
   def create
+    @user = User.find_by(username:params[:session][:username].downcase)
+    if @user
+      redirect_to @user
+    else
+      render 'new'
+    end 
   end
 
   def destroy
