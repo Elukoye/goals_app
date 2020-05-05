384557d69d667cc1e80171d013c5310e52d43148
Elukoye <lukoyedith@gmail.com>
2020-05-06 00:50:24 +0300

    modify project_spec.rb

--- spec/feature/project_spec.rb
+++ spec/feature/project_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 RSpec.feature "Project", :type => :feature do
-    before do
-        @user = User.create(username: 'example user')
-    end
-
     scenario "Creates a new project" do
         visit "/projects/new"
         fill_in "project[title]", :with => ""
@@ -12,11 +8,4 @@ RSpec.feature "Project", :type => :feature do
         click_button "Add New Project"
         expect(page).to have_content( "Project not created!")
     end
-
-#  scenario "Creates a new user" do
-#   visit "/users/new"
-#   fill_in "user[username]", :with => ""
-#   click_button "SignUp"
-#   expect(page).to have_content("Username can't be blank")
-# end
 end
