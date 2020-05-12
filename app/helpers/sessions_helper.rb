6cf07d89a5051fb375fb15f3e93e760aed19198d
Elukoye <lukoyedith@gmail.com>
2020-05-12 16:19:09 +0300

    modify helper/sessions file

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,9 +2,9 @@ module SessionsHelper
   # Returns the current logged-in user (if any)
   def current_user
     if @current_user.nil?
-       @current_user = User.find_by(id: session[:user_id])
+      @current_user = User.find_by(id: session[:user_id])
     else
-       @current_user
+      @current_user
     end
   end
 
